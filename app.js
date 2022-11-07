2872d1bcb8c98fd12520b4da5f8bce60e6017d0c
Maria Piper <mbtomori@users.noreply.github.com>
2022-11-07 14:30:36 -0800

    Complete part 4

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
 
-const app = express()
-app.get('/hello', (req, res) => {res.send('Life is good!')})
-app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+const app = express();
+app.use(express.json());
+
+HelloController(app);
+UserController(app);
+
 app.listen(4000)
+
